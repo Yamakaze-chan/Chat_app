9a467e0c85693e85ea7078e3b913ff5edbad091e
thaivo02 <2002.thaivo@gmail.com>
2023-01-11 21:51:13 +0700

    game ui

--- TCPSever/Game.Designer.cs
+++ TCPSever/Game.Designer.cs
@@ -30,45 +30,65 @@
         {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.title = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.grid = new System.Windows.Forms.DataGridView();
             this.session = new System.Windows.Forms.Timer(this.components);
-            this.button2 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.title.SuspendLayout();
+            this.title = new Guna.UI2.WinForms.Guna2GradientPanel();
             ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.title.SuspendLayout();
             this.SuspendLayout();
             // 
-            // title
+            // label2
             // 
-            this.title.BackColor = System.Drawing.Color.Lime;
-            this.title.Controls.Add(this.label2);
-            this.title.Controls.Add(this.button2);
-            this.title.Controls.Add(this.button1);
-            this.title.Controls.Add(this.label1);
-            this.title.Location = new System.Drawing.Point(0, 0);
-            this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(700, 100);
-            this.title.TabIndex = 0;
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(55)))), ((int)(((byte)(201)))));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(283, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 41);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Point:  0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(214)))), ((int)(((byte)(160)))));
+            this.button2.Location = new System.Drawing.Point(12, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "start game";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(613, 13);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(214)))), ((int)(((byte)(160)))));
+            this.button1.Location = new System.Drawing.Point(613, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 77);
+            this.button1.Size = new System.Drawing.Size(75, 33);
             this.button1.TabIndex = 1;
-            this.button1.Text = "X";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
-            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(279, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(418, 80);
+            this.label1.Size = new System.Drawing.Size(143, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Snake game";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -80,12 +100,10 @@
             this.grid.AllowUserToOrderColumns = true;
             this.grid.AllowUserToResizeColumns = false;
             this.grid.AllowUserToResizeRows = false;
-            this.grid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.grid.BackgroundColor = System.Drawing.Color.White;
-            this.grid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.grid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.grid.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable;
             this.grid.ColumnHeadersHeight = 29;
             this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.grid.ColumnHeadersVisible = false;
@@ -97,14 +115,15 @@
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Lime;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.grid.DefaultCellStyle = dataGridViewCellStyle2;
-            this.grid.Location = new System.Drawing.Point(0, 100);
+            this.grid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(209)))), ((int)(((byte)(102)))));
+            this.grid.Location = new System.Drawing.Point(0, 83);
             this.grid.MultiSelect = false;
             this.grid.Name = "grid";
             this.grid.ReadOnly = true;
             this.grid.RowHeadersVisible = false;
             this.grid.RowHeadersWidth = 51;
             this.grid.RowTemplate.Height = 24;
-            this.grid.Size = new System.Drawing.Size(700, 700);
+            this.grid.Size = new System.Drawing.Size(700, 458);
             this.grid.TabIndex = 1;
             this.grid.KeyUp += new System.Windows.Forms.KeyEventHandler(this.grid_KeyUp);
             // 
@@ -112,53 +131,46 @@
             // 
             this.session.Tick += new System.EventHandler(this.session_Tick);
             // 
-            // button2
+            // title
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(434, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(172, 33);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "start game";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(214)))), ((int)(((byte)(160)))));
+            this.title.Controls.Add(this.label1);
+            this.title.Controls.Add(this.button1);
+            this.title.Controls.Add(this.label2);
+            this.title.Controls.Add(this.button2);
+            this.title.Dock = System.Windows.Forms.DockStyle.Top;
+            this.title.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(71)))), ((int)(((byte)(111)))));
+            this.title.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(181)))), ((int)(((byte)(23)))), ((int)(((byte)(158)))));
+            this.title.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(0, 0);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(700, 82);
+            this.title.TabIndex = 3;
             // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(434, 49);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(172, 41);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Point:  0";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // Form2
+            // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 800);
+            this.ClientSize = new System.Drawing.Size(700, 541);
             this.ControlBox = false;
-            this.Controls.Add(this.grid);
             this.Controls.Add(this.title);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Name = "Form2";
-            this.Text = "Form2";
-            this.title.ResumeLayout(false);
+            this.Controls.Add(this.grid);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Game";
+            this.Text = "Snake and apples";
             ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.title.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel title;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView grid;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer session;
         private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2GradientPanel title;
     }
 }
